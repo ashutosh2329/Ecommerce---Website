ba9450677987cff9b11fc6bcb037e23c30f8249c
ashutosh.tiwari <ashutosh.tiwari@innovaccer.com>
2020-05-12 23:45:23 +0530

    Models created

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def store(request):
-	context = {}
+	products = Product.objects.all()
+	context = {'products':products}
 	return render(request, "store.html", context)
 
 def cart(request):
-	context = {}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+		order = {'get_cart_total':0,'get_cart_items':0}
+	context = {'items':items,'order':order}
 	return render(request, "cart.html", context)
 
 def checkout(request):
-	context = {}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+		order = {'get_cart_total':0,'get_cart_items':0}
+	context = {'items':items,'order':order}
 	return render(request, "checkout.html", context)
